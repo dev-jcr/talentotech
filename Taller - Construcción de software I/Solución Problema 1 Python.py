e57bada756ab29df25e5ddf7c707818019b511fc
Jonathan Cardona <khimerical@proton.me>
2025-09-23 20:12:45 -0500

    Primera entrada de tercer problema, inicio de código en Python

---
+++ Taller - Construcción de software I/Solución Problema 1 Python.py
@@ -0,0 +1,33 @@
+# Problema 1: Contar cuántos números pares hay en un vector
+# Descripción:
+#    • Crea un programa que reciba un vector de 8 números y cuente cuántos números pares hay en él.
+
+vectores[]
+vector=0
+pares=0
+
+# Recibir vector
+i=1
+mientras i < 8:
+   vector=input(f"Ingrese el valor {i} del vector")
+        si VECTOR I es NÚMERO
+            agregar VECTOR a VECTORES
+            I++
+         si no
+            imprimir "Por favor inserte solo números"
+            retornar
+
+# Contar valores pares en el vector
+mientras I > 0
+    si residuo VECTORES[I] - 1 / 2 =! 0
+        I++
+        retornar
+    si no
+        PARES++
+        I++
+        retornar
+
+# Imprimir resultado
+imprimir "La cantidad de números pares en el vector ingresado es PARES"
+
+        
